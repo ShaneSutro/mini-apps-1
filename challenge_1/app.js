748b88a815c88142b3ad42125d898d8522c275f4
Shane Sutro <shanesutro@gmail.com>
2021-01-24 10:49:01 -0700

    Create play board and begin selector logic

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,20 @@
+// var model = {
 
+// }
 
+// var view = {
+
+// }
+
+// var controller = {
+
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  board = document.querySelector("#board");
+  board.addEventListener('click', (e) => {
+    targetClass = e.path[0].className;
+    targetSpan = document.querySelector('.' + targetClass)
+    targetSpan.innerText = '[X]';
+  });
+});
