d48a7da4b4abeb0b3cc7b151d6c0614151dd65aa
Shane Sutro <shanesutro@gmail.com>
2021-01-25 21:30:29 -0700

    Finish setting up server and receiving posted data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const port = 3000;
 
+app.use(express.static('./client'))
+app.use(bodyParser.urlencoded( { extended: true }));
 
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+app.post('/', (req, res) => {
+  console.log(JSON.parse(req.body.text))
+  res.end('<h1>Done</h1>');
+})
+
+app.get('/text', (req, res) => {
+  res.sendFile(path.join(__dirname, '/samples/csv_report.csv'))
+})
+
+app.listen(port, (err) => {
+  console.log('Listening on port', port)
+})
