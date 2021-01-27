0b890bc9144b28771d9cac5587049675f6d6367f
Shane Sutro <shanesutro@gmail.com>
2021-01-26 21:15:35 -0700

    Return from server with CSV file

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 app.use(express.static('./client'))
@@ -12,8 +13,16 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(JSON.parse(req.body.text))
-  res.end('<h1>Done</h1>');
+  var data = JSON.parse(req.body.text);
+  var htmlReturn = parseData(data);
+  // stream.once('open', () => {
+  //   var string = keys.join(',') + '\n';
+  //   string += Object.values(data).join(',')
+  //   stream.write(string);
+  //   stream.end();
+  // })
+  const fileData = fs.readFileSync(path.join(__dirname, '/client/index.html'), { encoding: 'utf8' });
+  res.end(fileData + htmlReturn);
 })
 
 app.get('/text', (req, res) => {
@@ -23,3 +32,30 @@ app.get('/text', (req, res) => {
 app.listen(port, (err) => {
   console.log('Listening on port', port)
 })
+
+var parseData = (data) => {
+  var columns = [];
+  var rows = [];
+  var parseItem = (item) => {
+    var row = []
+    for (key in item) {
+      var index = columns.indexOf(`<th>${key}</th>`);
+      if (index < 0 && key !== 'children') {
+        columns.push(`<th>${key}</th>`)
+        index = columns.length - 1
+      }
+      if (key !== 'children') {
+        row.splice(index, 0, `<td>${item[key]}</td>`)
+      }
+    }
+    rows.push(`<tr>${row.join('')}</tr>`);
+
+    if (item.children && item.children.length > 0) {
+      for (var i = 0; i < item.children.length; i++) {
+        parseItem(item.children[i])
+      }
+    }
+  }
+  parseItem(data)
+  return `<table style="border: 1px solid black"><tr>${columns.join('')}</tr>${rows.join('')}</table>`
+}
