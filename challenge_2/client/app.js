502ea70fe214db4156154c87752d0112b393e6ef
Shane Sutro <shanesutro@gmail.com>
2021-01-27 20:33:32 -0700

    Complete Bare Minimum Requirements

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,19 @@
-
+$(document).ready(function () {
+  $('button').on('click', (e) => {
+    e.preventDefault();
+    var form = document.getElementById('form-id');
+    var formData = new FormData(form);
+    $.ajax({
+      url: 'http://localhost:3000/',
+      type: 'POST',
+      data: formData,
+      contentType: false,
+      processData: false,
+      success: (data, status, xhr) => {
+        $('body').html(data);
+        $('#download').css('display', 'inline');
+      }
+    })
+  })
+})
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -16,27 +16,19 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', upload.single('upload'), (req, res, next) => {
-  var data = req.body.text ? JSON.parse(req.body.text) : {};
   var multerFile = JSON.parse(Buffer.from(req.file.buffer).toString('utf8'));
-  console.log(multerFile);
   if (multerFile) {
     var htmlReturn = parseData(multerFile);
   }
-  // stream.once('open', () => {
-  //   var string = keys.join(',') + '\n';
-  //   string += Object.values(data).join(',')
-  //   stream.write(string);
-  //   stream.end();
-  // })
   const fileData = fs.readFileSync(path.join(__dirname, '/client/index.html'), {
     encoding: 'utf8',
   });
   res.end(fileData + htmlReturn);
 });
 
-app.get('/text', (req, res) => {
-  res.sendFile(path.join(__dirname, '/samples/csv_report.csv'));
-});
+app.get('/download', (req, res, next) => {
+  res.download(__dirname + '/client/report.csv', 'report.csv')
+})
 
 app.listen(port, (err) => {
   console.log('Listening on port', port);
@@ -44,20 +36,26 @@ app.listen(port, (err) => {
 
 var parseData = (data) => {
   var columns = [];
+  var csvHeaders = [];
   var rows = [];
+  var csvRows = [];
   var parseItem = (item) => {
     var row = [];
+    var csvRow = [];
     for (key in item) {
       var index = columns.indexOf(`<th>${key}</th>`);
       if (index < 0 && key !== 'children') {
         columns.push(`<th>${key}</th>`);
+        csvHeaders.push(key)
         index = columns.length - 1;
       }
       if (key !== 'children') {
+        csvRow.splice(index, 0, item[key]);
         row.splice(index, 0, `<td>${item[key]}</td>`);
       }
     }
     rows.push(`<tr>${row.join('')}</tr>`);
+    csvRows.push(csvRow.join(','));
 
     if (item.children && item.children.length > 0) {
       for (var i = 0; i < item.children.length; i++) {
@@ -66,7 +64,14 @@ var parseData = (data) => {
     }
   };
   parseItem(data);
-  return `<table style="border: 1px solid black"><tr>${columns.join(
-    ''
-  )}</tr>${rows.join('')}</table>`;
+  csvString = csvHeaders.join(',') + '\n' + csvRows.join('\n');
+  console.log('csvString: ', csvString);
+  fs.writeFile(path.join(__dirname, '/client/report.csv'), csvString, (err) => {
+    if (err) {
+      console.log('Error writing to file!', err);
+    } else {
+      console.log('Success!');
+    }
+  })
+  return `<table style="border: 1px solid black"><tr>${columns.join('')}</tr>${rows.join('')}</table>`;
 };
