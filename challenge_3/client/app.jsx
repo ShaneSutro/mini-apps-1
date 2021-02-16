5901c7455acce7c7672224852eedd86b1e7e98b0
Shane Sutro <shanesutro@gmail.com>
2021-02-15 20:04:50 -0700

    Set up server, React, and static file serving

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,8 @@
+'use strict';
 
+const App = (props) => (
+  <h1>Testing</h1>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const parser = require('body-parser');
+const PORT = 3000
 
+app.use(parser.json());
+app.use(express.static(path.join(__dirname, '/public')))
+console.log(path.join(__dirname, '/public'))
 
+app.get('/', (req, res) => {
+  res.sendFile('./index.html')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
