5421a7d9bdb5d1b348589c1fddb1d80db9fbe44b
Shane Sutro <shanesutro@gmail.com>
2021-02-17 20:53:18 -0700

    Setup webpack, mocha, chai, react

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const App = () => {
+  return (
+    <h1>This is a new message</h1>
+  )
 
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ challenge_4/spec/test.js
@@ -0,0 +1,21 @@
+var assert = require('assert')
+var expect = require('chai').expect;
+var should = require('chai').should()
+
+describe('Basic Test', () => {
+  xit('Should report 4 across as a win', () => {
+    //test
+  })
+
+  xit('Should report 4 stacked as a win', () => {
+    //test
+  })
+
+  xit('Should report a downward diagonal as a win', () => {
+    //test
+  })
+
+  xit('Should report an upward diagonal as a win', () => {
+    //test
+  })
+})
